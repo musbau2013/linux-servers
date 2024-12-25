d7b04cfe773769fa8ea3bb5d1f89c7d637d3e7aa
musbau2013 <musbauinfo@gmail.com>
2024-12-25 16:51:37 -0600

    Updated the workflow

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -39,25 +39,6 @@ resource "aws_iam_role" "ec2_role" {
   }
 
   # Attach an inline policy to allow CloudWatch Logs access
-  inline_policy {
-    name = "cloudwatch_logs_policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Action = [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
-            "logs:DescribeLogStreams"
-          ],
-          Effect   = "Allow",
-          Resource = "*"
-        }
-      ]
-    })
-  }
 }
 
 # Attach the AmazonSNSFullAccess policy to the IAM role (for SNS publishing permissions)
