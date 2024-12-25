70a47b0d6a87626ae780619297689a31a49b39dc
musbau2013 <musbauinfo@gmail.com>
2024-12-25 16:44:46 -0600

    Add ec2 module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,18 +1,8 @@
 # Configure the AWS provider
 #kgshfgkdfhjdkfjfld;
-provider "aws" {
-  region = "us-east-1" # Change to your desired AWS region
-}
 
-terraform {
-  required_version = "~>1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+
+
 
 # Create an IAM role for the EC2 instance
 ## gfhgjghij
@@ -118,74 +108,74 @@ resource "aws_sns_topic" "disk_space_topic" {
 }
 
 # Create an SNS subscription to send email notifications
-resource "aws_sns_topic_subscription" "email_subscription" {
-  topic_arn = aws_sns_topic.disk_space_topic.arn
-  protocol  = "email"
-  endpoint  = "musbauinfo@gmail.com" # Replace with your email address
-}
+# resource "aws_sns_topic_subscription" "email_subscription" {
+#   topic_arn = aws_sns_topic.disk_space_topic.arn
+#   protocol  = "email"
+#   endpoint  = "musbauinfo@gmail.com" # Replace with your email address
+# }
 
-# Create CloudWatch Alarms to monitor CPU utilization
-resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
-  alarm_name          = "CPUUtilizationAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 60
-  statistic           = "Average"
-  threshold           = 20
-  alarm_description   = "Alarm when CPU utilization exceeds 20%"
-  alarm_actions       = [aws_sns_topic.disk_space_topic.arn]
-  dimensions = {
-    InstanceId = aws_instance.example_instance.id
-  }
-}
+# # Create CloudWatch Alarms to monitor CPU utilization
+# resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
+#   alarm_name          = "CPUUtilizationAlarm"
+#   comparison_operator = "GreaterThanThreshold"
+#   evaluation_periods  = 1
+#   metric_name         = "CPUUtilization"
+#   namespace           = "AWS/EC2"
+#   period              = 60
+#   statistic           = "Average"
+#   threshold           = 20
+#   alarm_description   = "Alarm when CPU utilization exceeds 20%"
+#   alarm_actions       = [aws_sns_topic.disk_space_topic.arn]
+#   dimensions = {
+#     InstanceId = aws_instance.example_instance.id
+#   }
+# }
 
 # Create a CloudWatch Alarm to monitor disk space utilization
-resource "aws_cloudwatch_metric_alarm" "disk_space_alarm" {
-  alarm_name          = "DiskSpaceAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "DiskSpaceUtilization"
-  namespace           = "CWAgent"
-  period              = 60
-  statistic           = "Average"
-  threshold           = 35
-  alarm_description   = "Alarm when disk space utilization exceeds 35%"
-  alarm_actions       = [aws_sns_topic.disk_space_topic.arn]
-  dimensions = {
-    InstanceId = aws_instance.example_instance.id
-  }
-  treat_missing_data = "missing"
-}
+# resource "aws_cloudwatch_metric_alarm" "disk_space_alarm" {
+#   alarm_name          = "DiskSpaceAlarm"
+#   comparison_operator = "GreaterThanThreshold"
+#   evaluation_periods  = 1
+#   metric_name         = "DiskSpaceUtilization"
+#   namespace           = "CWAgent"
+#   period              = 60
+#   statistic           = "Average"
+#   threshold           = 35
+#   alarm_description   = "Alarm when disk space utilization exceeds 35%"
+#   alarm_actions       = [aws_sns_topic.disk_space_topic.arn]
+#   dimensions = {
+#     InstanceId = aws_instance.example_instance.id
+#   }
+#   treat_missing_data = "missing"
+# }
 
 # Create a CloudWatch Alarm to monitor memory utilization
-resource "aws_cloudwatch_metric_alarm" "memory_utilization_alarm" {
-  alarm_name          = "MemoryUtilizationAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "MemoryUtilization"
-  namespace           = "CWAgent"
-  period              = 60
-  statistic           = "Average"
-  threshold           = 15
-  alarm_description   = "Alarm when memory utilization exceeds 15%"
-  alarm_actions       = [aws_sns_topic.disk_space_topic.arn]
-  dimensions = {
-    InstanceId = aws_instance.example_instance.id
-  }
-  treat_missing_data = "missing"
-}
+# resource "aws_cloudwatch_metric_alarm" "memory_utilization_alarm" {
+#   alarm_name          = "MemoryUtilizationAlarm"
+#   comparison_operator = "GreaterThanThreshold"
+#   evaluation_periods  = 1
+#   metric_name         = "MemoryUtilization"
+#   namespace           = "CWAgent"
+#   period              = 60
+#   statistic           = "Average"
+#   threshold           = 15
+#   alarm_description   = "Alarm when memory utilization exceeds 15%"
+#   alarm_actions       = [aws_sns_topic.disk_space_topic.arn]
+#   dimensions = {
+#     InstanceId = aws_instance.example_instance.id
+#   }
+#   treat_missing_data = "missing"
+# }
 
-resource "aws_cloudwatch_log_group" "example_log_group" {
-  name              = "example-log-group" # Replace with your desired log group name
-  retention_in_days = 7                   # Set the desired retention period (in days)
-}
+# resource "aws_cloudwatch_log_group" "example_log_group" {
+#   name              = "example-log-group" # Replace with your desired log group name
+#   retention_in_days = 7                   # Set the desired retention period (in days)
+# }
 
-resource "aws_eip" "lb" {
-  instance = aws_instance.example_instance.id
-  domain   = "vpc"
-}
+# resource "aws_eip" "lb" {
+#   instance = aws_instance.example_instance.id
+#   domain   = "vpc"
+# }
 
 ##### API key for TMDB -   0069e2129562051768f17165f47d5d16
 
