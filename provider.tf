1f417c8597b371854885dd228b38256ff4e1b56d
musbau2013 <musbauinfo@gmail.com>
2024-12-25 18:05:15 -0600

    Updated providder.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,11 @@
 provider "aws" {
   region = "us-east-1" # Change to your desired AWS region
   default_tags {
-    tags = "testing"
+    tags = {
+      Name = "Jenkins"
+      Environment = "Dev"
+      Cost_Centre = "Tweakteck"
+
+    }
   }
 }
